7c7e7ea24399c90474b7e1c1d57be9a4579c60a9
xietsunzao <lordshinjo22@gmail.com>
2023-02-01 12:23:04 +0700

    refactor(Core): :recycle: Refactor Database change from constant property with .env key

--- App/Core/Database.php
+++ App/Core/Database.php
@@ -2,25 +2,22 @@
 
 namespace App\Core;
 
+use App\Core\DotEnvKey;
+
 use PDO;
 use PDOException;
 
 class Database
 {
-    private const DB_HOST  = 'localhost';
-    private const DB_USER = 'root';
-    private const DB_PASS = '';
-    private const DB_NAME = 'pdo';
-    private const DSN = 'mysql:host=' . self::DB_HOST . ';dbname=' . self::DB_NAME . '';
     public ?object $connection = null;
 
     public function __construct()
     {
         try {
             $this->connection = new PDO(
-                self::DSN,
-                self::DB_USER,
-                self::DB_PASS
+                "mysql:host=" . DotEnvKey::get('DB_HOST') . ";dbname=" . DotEnvKey::get('DB_NAME'),
+                DotEnvKey::get('DB_USER'),
+                DotEnvKey::get('DB_PASS')
             );
             $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
