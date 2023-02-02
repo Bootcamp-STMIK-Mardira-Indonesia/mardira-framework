eaa4c8da3d1b34461085f286d1a817b027f3afc3
xietsunzao <lordshinjo22@gmail.com>
2023-02-02 11:19:44 +0700

    refactor(Core): :recycle: Refactor query builder for chaining method use

--- App/Core/QueryBuilder.php
+++ App/Core/QueryBuilder.php
@@ -28,7 +28,9 @@ class QueryBuilder
     {
         $statement = $this->connection->prepare($query);
         $statement->execute();
-        return $this->statement;
+        $this->statement = $statement;
+        $this->buildQuery();
+        return $this;
     }
 
     public function select(array $columns = ['*'])
@@ -36,8 +38,8 @@ class QueryBuilder
         $columns = implode(', ', $columns);
         $query = "SELECT {$columns} FROM {$this->table}";
         $statement = $this->connection->prepare($query);
-        $statement->execute();
         $this->statement = $statement;
+        $this->buildQuery();
         return $this;
     }
 
@@ -47,53 +49,84 @@ class QueryBuilder
         $statement = $this->connection->prepare($query);
         $statement->execute();
         $this->statement = $statement;
+        $this->buildQuery();
         return $this;
     }
 
     public function where(string $column,  string $value, string $operator = '='): QueryBuilder
     {
-        $query = "SELECT * FROM {$this->table} WHERE {$column} {$operator} {$value}";
+        if (isset($this->statement->queryString)) {
+            $query = $this->statement->queryString;
+            $query .= " WHERE {$column} {$operator} {$value}";
+        } else {
+            $query = "SELECT * FROM {$this->table} WHERE {$column} {$operator} {$value}";
+        }
         $statement = $this->connection->prepare($query);
         $statement->execute();
         $this->statement = $statement;
+        $this->buildQuery();
         return $this;
     }
 
     public function whereIn(string $column, array $values): QueryBuilder
     {
         $values = implode(', ', $values);
-        $query = "SELECT * FROM {$this->table} WHERE {$column} IN ({$values})";
+        if (isset($this->statement->queryString)) {
+            $query = $this->statement->queryString;
+            $query .= " WHERE {$column} IN ({$values})";
+        } else {
+            $query = "SELECT * FROM {$this->table} WHERE {$column} IN ({$values})";
+        }
         $statement = $this->connection->prepare($query);
         $statement->execute();
         $this->statement = $statement;
+        $this->buildQuery();
         return $this;
     }
 
     public function whereNotIn(string $column, array $values): QueryBuilder
     {
         $values = implode(', ', $values);
-        $query = "SELECT * FROM {$this->table} WHERE {$column} NOT IN ({$values})";
+        if (isset($this->statement->queryString)) {
+            $query = $this->statement->queryString;
+            $query .= " WHERE {$column} NOT IN ({$values})";
+        } else {
+            $query = "SELECT * FROM {$this->table} WHERE {$column} NOT IN ({$values})";
+        }
         $statement = $this->connection->prepare($query);
         $statement->execute();
         $this->statement = $statement;
+        $this->buildQuery();
         return $this;
     }
 
     public function orderBy(string $column, string $order = 'ASC'): QueryBuilder
     {
-        $query = "SELECT * FROM {$this->table} ORDER BY {$column} {$order}";
+        if (isset($this->statement->queryString)) {
+            $query = $this->statement->queryString;
+            $query .= " ORDER BY {$column} {$order}";
+        } else {
+            $query = "SELECT * FROM {$this->table} ORDER BY {$column} {$order}";
+        }
         $statement = $this->connection->prepare($query);
         $statement->execute();
         $this->statement = $statement;
+        $this->buildQuery();
         return $this;
     }
 
     public function limit(int $limit)
     {
-        $query = "SELECT * FROM {$this->table} LIMIT {$limit}";
+        if (isset($this->statement->queryString)) {
+            $query = $this->statement->queryString;
+            $query .= " LIMIT {$limit}";
+        } else {
+            $query = "SELECT * FROM {$this->table} LIMIT {$limit}";
+        }
         $statement = $this->connection->prepare($query);
         $statement->execute();
         $this->statement = $statement;
+        $this->buildQuery();
         return $this;
     }
 
@@ -179,12 +212,24 @@ class QueryBuilder
         return $statement->execute();
     }
 
-    public function join(string $joinTable, string $column, string $joinColumn) : QueryBuilder
+    public function join(string $joinTable, string $column, string $joinColumn, string $joinType = NULL): QueryBuilder
     {
-        $query = "SELECT * FROM {$this->table} JOIN {$joinTable} ON {$this->table}.{$column} = {$joinTable}.{$joinColumn}";
+        $joinType = $joinType ? strtoupper($joinType) . ' JOIN' : 'JOIN';
+
+        if (isset($this->statement->queryString)) {
+            $query = "{$this->statement->queryString} {$joinType} {$joinTable} ON {$column} = {$joinColumn}";
+        } else {
+            $query = "SELECT * FROM {$this->table} {$joinType} {$joinTable} ON {$column} = {$joinColumn}";
+        }
         $statement = $this->connection->prepare($query);
         $statement->execute();
         $this->statement = $statement;
+        $this->buildQuery();
         return $this;
+    }
+
+    public function buildQuery(): string
+    {
+        return $this->statement->queryString;
     }
 }
