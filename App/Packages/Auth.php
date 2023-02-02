37ae1df29264f95e59d018b9e7390b12d756313b
xietsunzao <lordshinjo22@gmail.com>
2023-02-02 10:11:17 +0700

    refactor(Packages): :recycle: Add method for verify token check and users

--- App/Packages/Auth.php
+++ App/Packages/Auth.php
@@ -11,14 +11,37 @@ class Auth
 {
     use HasTokens;
 
+
+    private static function verify(string $token)
+    {
+        $user = (new self)->verifyToken($token, DotEnvKey::get('ACCESS_TOKEN_SECRET_KEY'));
+        if (isset($user->data)) {
+            return $user->data;
+        }
+        return false;
+    }
+
     public static function check(): bool
     {
         $token = Request::bearerToken();
-        $user = (new self)->verifyToken($token, DotEnvKey::get('ACCESS_TOKEN_SECRET_KEY'));
-        if (isset($user->data)) {
-            $user = QueryBuilder::table('users')->where('id', $user->data->id)->first();
+        $user = self::verify($token);
+        if ($user) {
+            $user = QueryBuilder::table('users')->where('id', $user->id)->first();
             if ($user) {
                 return true;
+            }
+        }
+        return false;
+    }
+
+    public static function user()
+    {
+        $token = Request::bearerToken();
+        $user = self::verify($token);
+        if ($user) {
+            $user = QueryBuilder::table('users')->where('id', $user->id)->first();
+            if ($user) {
+                return $user;
             }
         }
         return false;
