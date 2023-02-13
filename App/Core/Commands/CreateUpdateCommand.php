ead007f1a65a67d9528441999da07fdea0cc6ee3
xietsunzao <lordshinjo22@gmail.com>
2023-02-13 11:02:34 +0700

    refactor(Core): :recycle: Refactor CreateUpdateCommand

--- App/Core/Commands/CreateUpdateCommand.php
+++ App/Core/Commands/CreateUpdateCommand.php
@@ -18,7 +18,6 @@ class CreateUpdateCommand extends Command
     protected string $commandOptionName = "v";
     protected string $commandOptionDescription = 'Update to the specified version';
 
-
     protected function configure()
     {
         $this->setName($this->commandName)
@@ -34,35 +33,98 @@ class CreateUpdateCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output): void
     {
         $version = $input->getOption('version');
-
-        $this->update($version);
-
-        $output->writeln("<info>Application updated successfully.</info>");
-    }
-
-    protected function getStub(): string
-    {
-        return file_get_contents(__DIR__ . '/stubs/update.stub');
-    }
-
-    protected function getFileName(): string
-    {
-        return 'update';
-    }
-
-    protected function update($version): void
-    {
         $stub = $this->getStub();
         $stub = str_replace('{{version}}', $version, $stub);
-        // get file from packagist
         $file = file_get_contents('https://packagist.org/packages/mardira/mardira-framework');
-        // get version from file
         $version = $this->getVersion($file);
+        $this->removeLockFile();
+        $this->removeJsonFile();
+        $this->initComposer();
+        $this->requireCore($version);
 
-        $stub = str_replace('{{version}}', $version, $stub);
+        // get files from vendor folder app core
+        foreach (glob("vendor/mardira/mardira-framework/App/Core/*") as $file) {
 
-        // update core folder with new version
-        $this->updateCore($version);
+            // get command folder with if statement
+            if (basename($file) == 'Commands') {
+                // get files from command folder
+                foreach (glob("vendor/mardira/mardira-framework/App/Core/Commands/*") as $file) {
+                    // if file changed from vendor folder and from App/Core folder show message
+                    if (file_exists($file) && file_exists("App/Core/Commands/" . basename($file))) {
+                        // check difference between files from vendor folder and App/Core folder with --dif
+                        $diff = shell_exec("diff {$file} App/Core/Commands/" . basename($file));
+                        if ($diff) {
+                            //split file name from path mardira-framework
+                            $file = explode('mardira-framework', $file)[1];
+                            // text terminal color yellow and name file
+                            $textYellow = "\033[33m" . $file . "\033[0m";
+
+                            $output->writeln("<info>File {$textYellow} has been changed.</info>");
+                        }
+                    }
+
+                    // if file added from vendor folder and not exist in App/Core folder show message
+                    else if (file_exists($file) && !file_exists("App/Core/Commands/" . basename($file))) {
+                        //split file name from path mardira-framework
+                        $file = explode('mardira-framework', $file)[1];
+                        // text terminal color green and name file
+                        $textGreen = "\033[32m" . $file . "\033[0m";
+
+                        $output->writeln("<info>File {$textGreen} has been added.</info>");
+                    }
+
+                    // if file deleted from vendor folder and exist in App/Core folder show message
+                    else if (!file_exists($file) && file_exists("App/Core/Commands/" . basename($file))) {
+                        $file = implode('/', ['/App/Core/Commands', basename($file)]);
+                        // text terminal color red and name file
+                        $textRed = "\033[31m" . $file . "\033[0m";
+
+                        $output->writeln("<info>File {$textRed} has been deleted.</info>");
+                    }
+                }
+            }
+
+            // if file changed from vendor folder and from App/Core folder show message
+            if (file_exists($file) && file_exists("App/Core/" . basename($file))) {
+                // check difference between files from vendor folder and App/Core folder with --dif
+                $diff = shell_exec("diff {$file} App/Core/" . basename($file));
+                if ($diff) {
+                    //split file name from path mardira-framework
+                    $file = explode('mardira-framework', $file)[1];
+                    // text terminal color yellow and name file
+                    $textYellow = "\033[33m" . $file . "\033[0m";
+
+                    $output->writeln("<info>File {$textYellow} has been changed.</info>");
+                }
+            }
+
+            // if file added from vendor folder and not exist in App/Core folder show message
+            else if (file_exists($file) && !file_exists("App/Core/" . basename($file))) {
+                //split file name from path mardira-framework
+                $file = explode('mardira-framework', $file)[1];
+                // text terminal color green and name file
+                $textGreen = "\033[32m" . $file . "\033[0m";
+
+                $output->writeln("<info>File {$textGreen} has been added.</info>");
+            }
+
+            // if file deleted from vendor folder and exist in App/Core folder show message
+            else if (!file_exists($file) && file_exists("App/Core/" . basename($file))) {
+
+                //implode basename($file) with path /App/Core/
+                $file = implode('/', ['/App/Core', basename($file)]);
+                $textRed = "\033[31m" . $file . "\033[0m";
+
+                $output->writeln("<info>File {$textRed} has been deleted.</info>");
+            }
+        }
+        $this->removeCoreFolder();
+        $this->moveCoreFolder();
+        $this->moveJsonFile();
+        $this->removeVendorFolder();
+        $this->composerUpdate();
+
+        $output->writeln("<info>Application updated successfully.</info>");
     }
 
     protected function getVersion($file): string
@@ -76,43 +138,69 @@ class CreateUpdateCommand extends Command
         return $version;
     }
 
-    protected function updateCore($version): void
+
+    protected function removeLockFile(): void
     {
-        // remove composer.lock file
         $command = "rm -rf composer.lock";
         exec($command);
+    }
 
-        // remove composer.json file
+    protected function removeJsonFile(): void
+    {
         $command = "rm -rf composer.json";
         exec($command);
+    }
 
-        // composer init no interaction
+    protected function initComposer(): void
+    {
         $command = "composer init --no-interaction";
         exec($command);
+    }
 
-        // split v and version number
+    protected function requireCore($version): void
+    {
         $version = explode('v', $version)[1];
         $command = "composer require mardira/mardira-framework:{$version}";
         exec($command);
+    }
 
-        // remove old core folder
+    protected function removeCoreFolder(): void
+    {
         $command = "rm -rf App/Core";
         exec($command);
+    }
 
-        // move new core folder
+    protected function moveCoreFolder(): void
+    {
         $command = "mv vendor/mardira/mardira-framework/App/Core App";
-
         exec($command);
+    }
 
-        // move new composer.json file
+    protected function moveJsonFile(): void
+    {
         $command = "mv vendor/mardira/mardira-framework/composer.json .";
         exec($command);
+    }
 
+    protected function removeVendorFolder(): void
+    {
         $command = "rm -rf vendor";
         exec($command);
+    }
 
-        // run composer composer update
+    protected function composerUpdate(): void
+    {
         $command = "composer update";
         exec($command);
+    }
+
+    protected function getStub(): string
+    {
+        return file_get_contents(__DIR__ . '/stubs/update.stub');
+    }
+
+    protected function getFileName(): string
+    {
+        return 'update';
     }
 }
