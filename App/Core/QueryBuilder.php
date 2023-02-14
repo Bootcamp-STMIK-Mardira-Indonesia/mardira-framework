03330c703b382cd97c06c496b663c8ceca2a2c93
xietsunzao <lordshinjo22@gmail.com>
2023-02-14 17:27:38 +0700

    refactor(Core): :recycle: Refactor Query Builder

--- App/Core/QueryBuilder.php
+++ App/Core/QueryBuilder.php
@@ -11,154 +11,139 @@ class QueryBuilder
     public ?object $statement = null;
     public ?object $connection = null;
     protected string $table;
+    protected array $joins = [];
+    protected array $columns = [];
+    protected string $query = '';
+    protected array $where = [];
+    protected array $order = [];
+    protected int $limit = 0;
 
     public function __construct()
     {
         $this->connection = Database::getConnection();
     }
 
-    public static function table(string $table)
+    public static function table(string $table): QueryBuilder
     {
         $queryBuilder = new QueryBuilder();
         $queryBuilder->table = $table;
-        $query = "SELECT * FROM {$table}";
-        $statement = $queryBuilder->connection->prepare($query);
-        $statement->execute();
-        $queryBuilder->statement = $statement;
-        $queryBuilder->buildQuery();
         return $queryBuilder;
     }
 
-    public function query(string $query)
+    public function query(string $query): QueryBuilder
     {
-        $statement = $this->connection->prepare($query);
-        $statement->execute();
-        $this->statement = $statement;
-        $this->buildQuery();
+        $this->query = $query;
         return $this;
     }
 
-    public function select(array $columns = ['*'])
+    public function select(array $columns = ['*']): QueryBuilder
     {
-        $columns = implode(', ', $columns);
-        $query = "SELECT {$columns} FROM {$this->table}";
-        $statement = $this->connection->prepare($query);
-        $this->statement = $statement;
-        $this->buildQuery();
+        $this->columns = $columns;
         return $this;
     }
 
     public function from(string $table): QueryBuilder
     {
-        $query = "SELECT * FROM {$table}";
-        $statement = $this->connection->prepare($query);
-        $statement->execute();
-        $this->statement = $statement;
-        $this->buildQuery();
+        $this->table = $table;
         return $this;
     }
 
-    public function where(string $column,  string $value, string $operator = '='): QueryBuilder
+    public function where(string $column, $value, string $operator = '='): QueryBuilder
     {
-        if (isset($this->statement->queryString)) {
-            $query = $this->statement->queryString;
-            $query .= " WHERE {$column} {$operator} '{$value}'";
-        } else {
-            $query = "SELECT * FROM {$this->table} WHERE {$column} {$operator} '{$value}'";
-        }
-
-        $statement = $this->connection->prepare($query);
-        $statement->execute();
-        $this->statement = $statement;
-        $this->buildQuery();
+        $this->where[] = [
+            'column' => $column,
+            'value' => $value,
+            'operator' => $operator,
+        ];
         return $this;
     }
 
-    public function orWhere(string $column, string $value, string $operator = '='): QueryBuilder
+    public function orWhere(string $column, $value, string $operator = '='): QueryBuilder
     {
-        if (isset($this->statement->queryString)) {
-            $query = $this->statement->queryString;
-            $query .= " OR {$column} {$operator} '{$value}'";
-        } else {
-            $query = "SELECT * FROM {$this->table} OR {$column} {$operator} '{$value}'";
-        }
-        $statement = $this->connection->prepare($query);
-        $statement->execute();
-        $this->statement = $statement;
-        $this->buildQuery();
+        $this->where[] = [
+            'column' => $column,
+            'value' => $value,
+            'operator' => $operator,
+            'or' => true,
+        ];
         return $this;
     }
 
     public function whereIn(string $column, array $values): QueryBuilder
     {
-        $values = implode(', ', $values);
-        if (isset($this->statement->queryString)) {
-            $query = $this->statement->queryString;
-            $query .= " WHERE {$column} IN ('{$values}')";
-        } else {
-            $query = "SELECT * FROM {$this->table} WHERE {$column} IN ('{$values}')";
-        }
-        $statement = $this->connection->prepare($query);
-        $statement->execute();
-        $this->statement = $statement;
-        $this->buildQuery();
+        $this->where[] = [
+            'column' => $column,
+            'value' => $values,
+            'operator' => 'IN',
+        ];
         return $this;
     }
 
     public function whereNotIn(string $column, array $values): QueryBuilder
     {
-        $values = implode(', ', $values);
-        if (isset($this->statement->queryString)) {
-            $query = $this->statement->queryString;
-            $query .= " WHERE {$column} NOT IN ('{$values}')";
-        } else {
-            $query = "SELECT * FROM {$this->table} WHERE {$column} NOT IN ('{$values}')";
-        }
-        $statement = $this->connection->prepare($query);
-        $statement->execute();
-        $this->statement = $statement;
-        $this->buildQuery();
+        $this->where[] = [
+            'column' => $column,
+            'value' => $values,
+            'operator' => 'NOT IN',
+        ];
         return $this;
     }
 
-    public function orderBy(string $column, string $order = 'ASC'): QueryBuilder
+    public function whereNull(string $column): QueryBuilder
     {
-        if (isset($this->statement->queryString)) {
-            $query = $this->statement->queryString;
-            $query .= " ORDER BY {$column} {$order}";
-        } else {
-            $query = "SELECT * FROM {$this->table} ORDER BY {$column} {$order}";
-        }
-        $statement = $this->connection->prepare($query);
-        $statement->execute();
-        $this->statement = $statement;
-        $this->buildQuery();
+        $this->where[] = [
+            'column' => $column,
+            'value' => null,
+            'operator' => 'IS NULL',
+        ];
         return $this;
     }
 
-    public function limit(int $limit)
+    public function whereNotNull(string $column): QueryBuilder
     {
-        if (isset($this->statement->queryString)) {
-            $query = $this->statement->queryString;
-            $query .= " LIMIT {$limit}";
-        } else {
-            $query = "SELECT * FROM {$this->table} LIMIT {$limit}";
-        }
-        $statement = $this->connection->prepare($query);
-        $statement->execute();
-        $this->statement = $statement;
-        $this->buildQuery();
+        $this->where[] = [
+            'column' => $column,
+            'value' => null,
+            'operator' => 'IS NOT NULL',
+        ];
         return $this;
     }
 
-    public function get(): array
+    public function orderBy(string $column, string $direction = 'ASC'): QueryBuilder
     {
-        return $this->statement->fetchAll(PDO::FETCH_OBJ);
+        $this->order[] = [
+            'column' => $column,
+            'direction' => $direction,
+        ];
+        return $this;
     }
 
-    public function first()
+    public function orderByAsc(string $column): QueryBuilder
     {
-        return $this->statement->fetch(PDO::FETCH_OBJ);
+        return $this->orderBy($column, 'ASC');
+    }
+
+    public function orderByDesc(string $column): QueryBuilder
+    {
+        return $this->orderBy($column, 'DESC');
+    }
+
+    public function join(string $table, string $first, string $second, string $type = ''): QueryBuilder
+    {
+        $this->joins[] = [
+            'table' => $table,
+            'first' => $first,
+            'operator' => '=',
+            'second' => $second,
+            'type' => $type,
+        ];
+        return $this;
+    }
+
+    public function limit(int $limit): QueryBuilder
+    {
+        $this->limit = $limit;
+        return $this;
     }
 
     public function insert($data): bool
@@ -191,6 +176,24 @@ class QueryBuilder
         }
     }
 
+    public function get(): array
+    {
+        $query = $this->buildQuery();
+
+        $this->statement = $this->connection->prepare($query);
+        $this->bindValues();
+        $this->statement->execute();
+        return $this->statement->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    public function first()
+    {
+        $query = $this->buildQuery();
+        $this->statement = $this->connection->prepare($query);
+        $this->bindValues();
+        $this->statement->execute();
+        return $this->statement->fetch(PDO::FETCH_OBJ);
+    }
 
     public function update(array $data): bool
     {
@@ -216,35 +219,111 @@ class QueryBuilder
         return $statement->execute();
     }
 
+    public function buildQuery(): string
+    {
+        $query = "SELECT ";
+
+        if (count($this->columns) > 0) {
+            $query .= implode(', ', $this->columns);
+        } else {
+            $query .= '*';
+        }
+
+        $query .= " FROM {$this->table}";
+
+        if (count($this->joins) > 0) {
+            foreach ($this->joins as $join) {
+                $query .= " {$join['type']} JOIN {$join['table']} ON {$join['first']} {$join['operator']} {$join['second']}";
+            }
+        }
+
+        if (count($this->where) > 0) {
+            $query .= ' WHERE ';
+            foreach ($this->where as $key => $where) {
+                if (is_array($where['value'])) {
+                    $query .= "{$where['column']} {$where['operator']} (";
+                    foreach ($where['value'] as $value) {
+                        //remove reference column name for binding a.id to id
+                        if (strpos($where['column'], '.') !== false) {
+                            // change to _
+                            $columns = str_replace('.', '_', $where['column']);
+                        } else {
+                            $columns = $where['column'];
+                        }
+                        $query .= ":{$columns}, ";
+                    }
+                    $query = rtrim($query, ', ');
+                    $query .= ')';
+                } else {
+                    //remove reference column name for binding a.id to id
+                    if (strpos($where['column'], '.') !== false) {
+                        // change to _
+                        $columns = str_replace('.', '_', $where['column']);
+                    } else {
+                        $columns = $where['column'];
+                    }
+
+                    $query .= "{$where['column']} {$where['operator']} :{$columns}";
+                }
+                if ($key < count($this->where) - 1) {
+                    // if isset or
+                    $query .= ' AND ';
+                }
+            }
+        }
+
+        if (count($this->order) > 0) {
+            $query .= ' ORDER BY ';
+            foreach ($this->order as $key => $order) {
+                $query .= "{$order['column']} {$order['direction']}";
+                if ($key < count($this->order) - 1) {
+                    $query .= ', ';
+                }
+            }
+        }
+
+        if ($this->limit > 0) {
+            $query .= " LIMIT {$this->limit}";
+        }
+
+        return $query;
+    }
+
+    public function bindValues()
+    {
+
+        // fix Invalid bind parameter with alias column
+
+        foreach ($this->where as $where) {
+            if (is_array($where['value'])) {
+                foreach ($where['value'] as $value) {
+                    //remove reference column name for binding a.id to id
+                    if (strpos($where['column'], '.') !== false) {
+                        // change to _
+                        $columns = str_replace('.', '_', $where['column']);
+                    } else {
+                        $columns = $where['column'];
+                    }
+                    $this->statement->bindValue(':' . $columns, $value);
+                }
+            } else {
+                //remove reference column name for binding a.id to id
+                if (strpos($where['column'], '.') !== false) {
+                    // change to _
+                    $columns = str_replace('.', '_', $where['column']);
+                } else {
+                    $columns = $where['column'];
+                }
+                $this->statement->bindValue(':' . $columns, $where['value']);
+            }
+        }
+    }
+
     public function delete(string $column, string $value): bool
     {
         $query = "DELETE FROM {$this->table} WHERE {$column} = :value";
         $statement = $this->connection->prepare($query);
         $statement->bindValue(':value', $value);
         return $statement->execute();
-    }
-
-    public function join(string $joinTable, string $column, string $joinColumn, string $joinType = NULL): QueryBuilder
-    {
-        $joinType = $joinType ? strtoupper($joinType) . ' JOIN' : 'JOIN';
-
-
-        // if join multiple tables
-        if (isset($this->statement->queryString)) {
-            $query = $this->statement->queryString;
-            $query .= " {$joinType} {$joinTable} ON {$column} = {$joinColumn}";
-        } else {
-            $query = "SELECT * FROM {$this->table} {$joinType} {$joinTable} ON {$column} = {$joinColumn}";
-        }
-        $statement = $this->connection->prepare($query);
-        $statement->execute();
-        $this->statement = $statement;
-        $this->buildQuery();
-        return $this;
-    }
-
-    public function buildQuery(): string
-    {
-        return $this->statement->queryString;
     }
 }
