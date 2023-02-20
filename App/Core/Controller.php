7d11db99842100a91ef4df43d8f1b9ab172b5242
xietsunzao <lordshinjo22@gmail.com>
2023-02-21 00:34:06 +0700

    fix(Core): :bug: Fix CORS integration API

--- App/Core/Controller.php
+++ App/Core/Controller.php
@@ -2,10 +2,16 @@
 
 namespace App\Core;
 
-header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-header('Access-Control-Allow-Headers: X-Requested-With');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Max-Age: 86400');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 
 use App\Core\Input;
 use App\Core\DotEnvKey;
