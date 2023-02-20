6bf87ee6613b5d9f73c72174c5760a6271a01d83
xietsunzao <lordshinjo22@gmail.com>
2023-02-21 00:52:34 +0700

    fix(Core): :bug: Fix CORS Response Code

--- App/Core/Responses.php
+++ App/Core/Responses.php
@@ -2,6 +2,20 @@
 
 namespace App\Core;
 
+// solve CORS header
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Max-Age: 86400');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
+
+
 trait Responses
 {
 
