f33a1e69c86e004a60b8693865a9d9208d78191b
xietsunzao <lordshinjo22@gmail.com>
2023-02-21 02:34:37 +0700

    refactor(Core): :fire: Remove headers on main controller

--- App/Core/Controller.php
+++ App/Core/Controller.php
@@ -2,17 +2,6 @@
 
 namespace App\Core;
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
-header('Access-Control-Allow-Credentials: true');
-header('Access-Control-Max-Age: 86400');
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit;
-}
-
 use App\Core\Input;
 use App\Core\DotEnvKey;
 use App\Core\HasTokens;
